47500df26df2c23f55bfca7c114bafd7832d9c19
pung-one <p-ungerer@systemli.org>
2023-02-22 14:05:44 +0100

    add spotlight component

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,33 @@
 import useSWR from "swr";
 import ArtPieces from "../components/ArtPieces";
+import Spotlight from "../components/Spotlight";
 import { URL } from "./_app";
+import { useEffect, useState } from "react";
+import { useImmer } from "use-immer";
 
 export default function HomePage() {
   const { data, error, isLoading } = useSWR(URL);
   if (error) return <h1>failed to load</h1>;
   if (isLoading) return <h1>loading...</h1>;
 
-  console.log(data);
+  /* const [dailyImage, updateDailyImage] = useImmer({});
+  const d = new Date().getDate(); */
+
+  const { imageSource, name, artist } =
+    data[Math.floor(Math.random() * data.length)];
+
+  /* useEffect(() => {
+    updateDailyImage((draft) => {
+      draft.image = imageSource;
+      draft.title = name;
+      draft.artist = artist;
+    });
+  }, [d]); */
 
   return (
     <div>
-      <ArtPieces pieces={data} />
+      {/* <ArtPieces pieces={data} /> */}
+      <Spotlight image={imageSource} title={name} artist={artist} />
     </div>
   );
 }
